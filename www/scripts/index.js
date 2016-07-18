497ba45c6f5d9414bb7aee19b20e2903095e4e52
Alexander Raud <araud@mail.ru>
2016-07-18 21:06:44 +0300

    notification

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -5,22 +5,52 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+
+        boom();
+
         browser = cordova.InAppBrowser.open('http://365word.ru', '_parent', 'location=no')
         browser.addEventListener('exit', onBrowserExit);
     };
+
+    function bringToFront()
+    {
+
+    }
+
+    function boom()
+    {
+        /*
+        navigator.notification.alert(
+            'You are the winner!',  // message
+            onBrowserExit,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+        */
+        var today = new Date();
+        today.setMinutes(today.getMinutes() + 1);
+        cordova.plugins.notification.local.schedule({
+            id: 1,
+            text: 'My first notification',
+            every: 'day',
+            firstAt: today,
+        });
+        cordova.plugins.notification.local.on('click', function (notification) {
+            onBrowserExit();
+        }, this);
+    }
 
     function onBrowserExit()
     {
@@ -29,9 +59,11 @@
 
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
+        boom();
     };
 
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
+        boom();
     };
 } )();
